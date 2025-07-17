9db0ea4f5448304e5312fa1f96b04118cb1f077e
vicmikon <zhenglun.vicwu@gmail.com>
2025-07-17 09:22:33 +0800

    Update 7458.v

--- 7458.v
+++ 7458.v
@@ -4,9 +4,9 @@ module top_module (
     input p2a, p2b, p2c, p2d,
     output p2y );
 	
-		wire p1_abc, p1_edf, p2_ab, p2_cd;
-		assign {p1_abc, p1_edf, p2_ab, p2_cd} = {(p1a & p1b & p1c), (p1e & p1d & p1f), (p2a & p2b), (p2c & p2d)};
-	  assign p1y = p1_abc | p1_edf;
-	  assign p2y = p2_ab | p2_cd;
+    wire p1_abc, p1_edf, p2_ab, p2_cd;
+    assign {p1_abc, p1_edf, p2_ab, p2_cd} = {(p1a & p1b & p1c), (p1e & p1d & p1f), (p2a & p2b), (p2c & p2d)};
+    assign p1y = p1_abc | p1_edf;
+    assign p2y = p2_ab | p2_cd;
     
 endmodule
