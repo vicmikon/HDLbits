17e05f7c3792d1c6109287ee4c0609edd1752e66
vicmikon <zhenglun.vicwu@gmail.com>
2025-07-17 17:47:20 +0800

    Update Popcount255.v

--- Popcount255.v
+++ Popcount255.v
@@ -3,14 +3,14 @@ module top_module(
     output [7:0] out );
 	
     integer i;
-    
     always @(*) begin
         out = 0;
-        for(i = 0; i < 255; i = i + 1)
+        for(i = 0; i < 255; i = i + 1) begin
             if(in[i])
                 out = out + 1'd1;
-        	else
+            else
                 out = out;
+        end
     end
     
 endmodule
