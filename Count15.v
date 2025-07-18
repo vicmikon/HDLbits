c71c06fd6179248c94ba4851d72e6894c0fe2637
vicmikon <zhenglun.vicwu@gmail.com>
2025-07-18 09:58:46 +0800

    Create Count15.v

---
+++ Count15.v
@@ -0,0 +1,18 @@
+module top_module (
+    input clk,
+    input reset,      // Synchronous active-high reset
+    output reg [3:0] q);
+
+    always @(posedge clk) begin
+        if (reset) begin
+            q <= 0;
+        end
+        else begin
+            if (q < 16)
+                q <= q + 1;
+            else
+                q <= q;
+        end
+    end
+        
+endmodule
