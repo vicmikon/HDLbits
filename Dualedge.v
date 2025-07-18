a694b084982e836bdc9e39f3b73c7d97879f0aba
vicmikon <zhenglun.vicwu@gmail.com>
2025-07-18 09:58:11 +0800

    Create Dualedge.v

---
+++ Dualedge.v
@@ -0,0 +1,19 @@
+module top_module (
+    input clk,
+    input d,
+    output q
+);
+
+    reg p, n;
+    
+    always @(posedge clk) begin
+        p <= d ^ n;
+    end
+    
+    always @(negedge clk) begin
+        n <= d ^ p;
+    end
+    
+    assign q = p ^ n;
+    
+endmodule
